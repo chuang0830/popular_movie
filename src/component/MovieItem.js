d8cca682df30cc1d14abbcf3ab8da259b11ed20c
chuang0830 <eilleen50830@gmail.com>
2022-09-14 10:21:51 +0800

    img lazyload

--- src/component/MovieItem.js
+++ src/component/MovieItem.js
@@ -6,7 +6,11 @@ const MovieItem =({value})=>{
         <>
             <Link className="movie_item" to={title?`/movie/${id}`:`/tv/${id}`}>
                 <div className="movie_content">
-                    <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`} alt="" />
+                    <img loading="lazy"
+                    src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`}
+                    srcSet={`https://image.tmdb.org/t/p/w220_and_h330_face${poster_path} 1x,
+                    https://image.tmdb.org/t/p/w440_and_h660_face/${poster_path} 2x`} 
+                    alt= {title||name}/>
                     <div className="rating">
                         {vote_average}
                     </div>
