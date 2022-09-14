f2df7a8878bc5af3cede1db22f24c5aeda20a396
chuang0830 <eilleen50830@gmail.com>
2022-09-14 22:45:23 +0800

    category for actor

---
+++ src/component/PersonItem.js
@@ -0,0 +1,14 @@
+const PersonItem =({value})=>{
+    const { profile_path,name }=value
+    return(<>
+    <div className="person_item">
+        <div className="person_img">
+            <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${profile_path}`} alt="" />
+        </div>
+        <div className="person_name">
+            {name}
+        </div>
+    </div>
+    </>)
+}
+export default PersonItem
--- src/page/Search.js
+++ src/page/Search.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {searchItem, removeSearch} from '../redux/action/search'
 import { useDispatch, useSelector } from "react-redux";
 import MovieItem from "../component/MovieItem";
+import PersonItem from "../component/PersonItem";
+import Loading from "../component/Loading";
 
 const Search =()=>{
+  const cateshow = ['movie','tv']
+  const cateperson = ['person']
+  const [ cate, setCate ] = useState(cateperson)
   const { searchInput } = useParams();
     const dispatch = useDispatch();
     const searchItems = useSelector(state=>state.searchItem.items)
@@ -15,10 +20,24 @@ const Search =()=>{
         };
       }, [searchInput]);
     return(<>
-          <div className="searchList_wrap" style={{flexWrap:'wrap'}}>
-            {searchItems&&searchItems.length&&searchItems.map(value=>{
-              return value.backdrop_path && <MovieItem value={value} key={value.id}/>
-            })}
+            <div className="search">
+                <div className="category">
+                  <span className={cate.includes('tv')?'active':''}
+                  onClick={()=>setCate(cateshow)}
+                  >片名</span>
+                  <span className={cate.includes('person')?'active':''}
+                   onClick={()=>setCate(cateperson)}
+                  >演員</span>
+                </div>
+                <div className="searchList_wrap" style={{flexWrap:'wrap'}}>
+                  {searchItems&&searchItems.length?searchItems.map(value=>{
+                    return cate.includes('person')?
+                    cate.includes(value.media_type)&&
+                    <PersonItem value={value} key={value.id}/>:
+                    cate.includes(value.media_type)&&
+                    <MovieItem value={value} key={value.id}/>
+                  }):searchItems?<h2>找不到符合的結果</h2>:<Loading/>}
+                  </div>
             </div>
           </>)
 }
